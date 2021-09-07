235e0aaabc188ac48135dcf5775ff803c218111f
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-09-07 17:21:47 +0800

    feat: Progress for video wall page

---
+++ src/components/video-wall.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="video-wall">
+            <div class="columns">
+                <div class="column is-one-fifth" id="tmp">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth" id="tmp">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth" id="tmp">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+                <div class="column is-one-fifth">
+                    <figure class="image is-5by4">
+                        <img src="https://bulma.io/images/placeholders/256x256.png">
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.video-wall{
+    max-width: 60%;
+    margin: 3em;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import homepage from './components/home-page.vue'
+import videowall from './components/video-wall.vue'
 
 export default[
-    {path:'/', component: homepage}
+    {path:'/', component: homepage},
+    {path:'/videowall', component: videowall}
 ]
