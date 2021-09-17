f7e9fb4cfd62147dea8fb0fc4c369331ebd5618d
kshitiz675 <kshitiz@live.ca>
2021-09-17 19:34:10 +0800

    projectapp fix + rest framework

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projectapp.app.MyappConfig'
+    'projectapp.apps.ProjectappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
