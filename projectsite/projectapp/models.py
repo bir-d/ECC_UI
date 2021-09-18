1b3d44751da2a3b33ebb0b27bc6d7a82e483363a
kshitiz675 <kshitiz@live.ca>
2021-09-18 16:21:25 +0800

    Models Update

--- projectsite/projectapp/models.py
+++ projectsite/projectapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
 
@@ -6,3 +7,31 @@ from django.db import models
 class testEntry(models.Model):
     title = models.CharField(max_length=100)
     line1 = models.CharField(max_length=100)
+
+class Lights(models.Model):
+    light_id = models.PositiveIntegerField(max_length=100)
+    name = models.CharField(max_length=255)
+    colour = models.PositiveIntegerField()
+    brightness = models.PositiveIntegerField()
+    light_on = models.BooleanField()
+    selected = models.BooleanField()
+
+class Lights_Group(models.Model):
+    id = models.PositiveIntegerField(max_length=2)
+    group_name = models.CharField(max_length=255)
+    colour = models.PositiveIntegerField()
+    brightness = models.PositiveIntegerField()
+    group_on = models.BooleanField()
+    selected = models.BooleanField()
+
+class Video_Wall_Panel(models.Model):
+    panel_id = models.PositiveIntegerField()
+    panel_on = models.BooleanField()
+
+class Video_Wall_Panel_Group(models.Model):
+    id = models.PositiveIntegerField(max_length=1)
+    group_on = models.BooleanField()
+
+class Workstations(models.Model):
+    workstation_id = models.PositiveIntegerField(max_length=1)
+    station_on = models.BooleanField()
