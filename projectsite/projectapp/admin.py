bbf5757753a0ce48b0c9989fb9b588bf85674779
kshitiz675 <kshitiz@live.ca>
2021-09-19 22:40:18 +0800

    Database Migrations

--- projectsite/projectapp/admin.py
+++ projectsite/projectapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import testEntry
+from projectapp.models import Lights
+from projectapp.models import Lights_Group
+from projectapp.models import Video_Wall_Panel
+from projectapp.models import Video_Wall_Panel_Group
+from projectapp.models import Workstations
+
 # Register your models here.
 
-admin.site.register(testEntry)
+admin.site.register(Lights_Group) 
+admin.site.register(Lights)
+admin.site.register(Video_Wall_Panel)
+admin.site.register(Video_Wall_Panel_Group)
+admin.site.register(Workstations)
--- projectsite/projectapp/models.py
+++ projectsite/projectapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 
 class Lights_Group(models.Model):
-    id = models.PositiveIntegerField(max_length=2)
+    id = models.PositiveIntegerField(primary_key=True)
     group_name = models.CharField(max_length=255)
     colour = models.PositiveIntegerField()
     brightness = models.PositiveIntegerField()
@@ -10,23 +10,23 @@ class Lights_Group(models.Model):
     selected = models.BooleanField()
 
 class Lights(models.Model):
-    light_id = models.PositiveIntegerField(max_length=100)
+    id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     colour = models.PositiveIntegerField()
     brightness = models.PositiveIntegerField()
     light_on = models.BooleanField()
     selected = models.BooleanField()
-    group_id = models.ForeignKey(Lights_Group)
+    group_id = models.ForeignKey(Lights_Group, on_delete=models.CASCADE)
 
 class Video_Wall_Panel_Group(models.Model):
-    id = models.PositiveIntegerField(max_length=1)
+    id = models.PositiveIntegerField(primary_key=True)
     group_on = models.BooleanField()
 
 class Video_Wall_Panel(models.Model):
-    panel_id = models.PositiveIntegerField()
+    id = models.PositiveIntegerField(primary_key=True)
     panel_on = models.BooleanField()
-    video_wall_panel_group_id = models.ForeignKey(Video_Wall_Panel_Group)
+    video_wall_panel_group_id = models.ForeignKey(Video_Wall_Panel_Group, on_delete=models.CASCADE)
 
 class Workstations(models.Model):
-    workstation_id = models.PositiveIntegerField(max_length=1)
+    id = models.PositiveIntegerField(primary_key=True)
     station_on = models.BooleanField()
