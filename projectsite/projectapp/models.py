78876b1c1d9da91ac3bc0ad35782c6f52d9f8ded
kshitiz675 <kshitiz@live.ca>
2021-09-19 17:16:38 +0800

    Models Update

--- projectsite/projectapp/models.py
+++ projectsite/projectapp/models.py
@@ -1,21 +1,6 @@
 from django.db import models
 from django.db.models.base import Model
 
-# # Create your models here.
-
-# # example model
-# class testEntry(models.Model):
-#     title = models.CharField(max_length=100)
-#     line1 = models.CharField(max_length=100)
-
-class Lights(models.Model):
-    light_id = models.PositiveIntegerField(max_length=100)
-    name = models.CharField(max_length=255)
-    colour = models.PositiveIntegerField()
-    brightness = models.PositiveIntegerField()
-    light_on = models.BooleanField()
-    selected = models.BooleanField()
-
 class Lights_Group(models.Model):
     id = models.PositiveIntegerField(max_length=2)
     group_name = models.CharField(max_length=255)
@@ -24,13 +9,23 @@ class Lights_Group(models.Model):
     group_on = models.BooleanField()
     selected = models.BooleanField()
 
-class Video_Wall_Panel(models.Model):
-    panel_id = models.PositiveIntegerField()
-    panel_on = models.BooleanField()
+class Lights(models.Model):
+    light_id = models.PositiveIntegerField(max_length=100)
+    name = models.CharField(max_length=255)
+    colour = models.PositiveIntegerField()
+    brightness = models.PositiveIntegerField()
+    light_on = models.BooleanField()
+    selected = models.BooleanField()
+    group_id = models.ForeignKey(Lights_Group)
 
 class Video_Wall_Panel_Group(models.Model):
     id = models.PositiveIntegerField(max_length=1)
     group_on = models.BooleanField()
+
+class Video_Wall_Panel(models.Model):
+    panel_id = models.PositiveIntegerField()
+    panel_on = models.BooleanField()
+    video_wall_panel_group_id = models.ForeignKey(Video_Wall_Panel_Group)
 
 class Workstations(models.Model):
     workstation_id = models.PositiveIntegerField(max_length=1)
