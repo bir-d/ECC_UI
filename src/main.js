5df174bbe8f4e21dd7c886c5cca4f1a8b5aa28df
bird <20701908+bir-d@users.noreply.github.com>
2021-09-20 18:03:34 +0800

    Add font awesome?

--- src/main.js
+++ src/main.js
@@ -5,7 +5,30 @@ import 'buefy/dist/buefy.css'
 import VueRouter from 'vue-router'
 import Routes from './routes'
 
-Vue.use(Buefy)
+// FA, add icons you need to the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+
+
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas",
+  customIconPacks: {
+    fas: {
+      sizes: {
+        default: "lg",
+        "is-small": "1x",
+        "is-medium": "2x",
+        "is-large": "3x"
+      },
+      iconPrefix: ""
+    }
+  }
+})
+
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false;
