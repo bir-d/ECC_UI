d55c02dc99741389fc8b8e0c4eb6ec99a5c20a47
Computer <computer@Computers-MacBook-Pro.local>
2021-10-04 15:36:00 +0800

    Added Frontend To Backend Meshing
    Lights page now uses backend db, feels kinda scuffed. But hey, it works.

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,10 @@ import homepage from './components/home-page.vue'
 import videowall from './components/video-wall.vue'
 import lightspage from './components/light-page.vue'
 import displays from './components/display'
-import tests from './components/test.vue'
-import tests2 from './components/test2.vue'
 
 export default[
     {path:'/', component: homepage},
     {path:'/videowall', component: videowall},
     {path:'/lights', component: lightspage},
     {path:'/display',component: displays},
-    {path:'/test',component: tests},
-    {path:'/test2',component: tests2}
 ]
