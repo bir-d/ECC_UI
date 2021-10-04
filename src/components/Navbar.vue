d8602bd1f025a67d5bdd35861f4316c058e1948f
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-10-05 07:34:52 +0800

    fix: Remove navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,18 @@
 <template>
-  <div class="hello">
+  <div>
     <b-navbar>
         <template #brand>
             <b-navbar-brand tag="router-link" :to="{ path: '/' }" id="brand">
+            <figure class="image is-128x128">
                 <a href="/"><img src="../assets/navbar-thales-logo.png" alt="Thales Logo"></a>
+            </figure>
             </b-navbar-brand>
         </template>
-        <template #start>
-            <b-navbar-item href="/videowall">
-                <p id="navbar">Video Wall</p>
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                <p id="navbar">Workstations</p>
-            </b-navbar-item>
-            <b-navbar-item href="/lights">
-                <p id="navbar">Lights</p>
-            </b-navbar-item>
-            <b-navbar-item href="display">
-                <p id="navbar">Displays</p>
-            </b-navbar-item>
-        </template>
-
         <template #end>
             <b-navbar-item tag="div">
                 <div class="buttons">
-                    <a class="button">
+                    <a class="button is-large">
                         <p id="button-text">+ Preset</p>
-                    </a>
-                </div>
-            </b-navbar-item>
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                    <a class="button">
-                        <p id="button-text">Settings</p>
                     </a>
                 </div>
             </b-navbar-item>
@@ -42,17 +22,6 @@
 </template>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
@@ -64,14 +33,11 @@ a {
 #button-text{
     font-family: monospace;
     color: white;
-    
 }
 .button{
     background-color:#6b705c;
 }
 #brand{
-    height: 60px;
-    width: 60px;
     margin-left: 25px;
     margin-right: 25px;
 }
