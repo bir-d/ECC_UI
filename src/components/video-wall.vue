93bf37fbec9eb5f7b6d3c5abab394477363c08a0
Toh Jianzhen <tohjianzhen1@gmail.com>
2021-10-05 13:57:50 +0800

    feat: Video Wall progress

--- src/components/video-wall.vue
+++ src/components/video-wall.vue
@@ -2,75 +2,16 @@
     <div>
         <div class="columns">
             <div class="column is-half">
-                <b-field class="video-wall">
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                </b-field>
-                <b-field class="video-wall">
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                </b-field>
-                <b-field class="video-wall">
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                    <b-upload v-model="dropFiles" multiple drag-drop expanded class="wall-piece">
-                        <section class="section">
-                        <div class="content has-text-centered">
-                            <p>Drop your files here</p>
-                        </div>
-                        </section>
-                    </b-upload>
-                </b-field>
+                <div class="video-wall">
+                    <div class="upload">
+                        <input type="file" @change="onFileChange">
+                    </div>
+                    <div class="video-player">
+                        <video controls>
+                            <source type="video/mp4" v-for="(videos,index) in dropFiles" :key="index" class="tag is-primary" src=videos.address />
+                        </video>
+                    </div>
+                </div>
                 <div class="tags">
                     <span v-for="(file, index) in dropFiles" :key="index" class="tag is-primary">
                         {{file.name}}
@@ -192,8 +133,20 @@
 </template>
 
 <style scoped>
+.title{
+    margin-top: 1em;
+    margin-bottom: 1.5em;
+}
 .video-wall{
     margin-top: 3em;
+    margin-left: 2.5em;
+}
+.upload{
+    margin-bottom: 2.5em;
+}
+.video-player{
+    width: 100%;
+    height: 20%;
 }
 .wall-piece{
     margin-left: 1em;
@@ -206,13 +159,10 @@
     margin-top: 3em;
     margin-left: 7em;
 }
-
 .filename{
     opacity: 75%;
 }
-
 .files{
-    border-style: dashed;
     border-radius: 5px;
     /* RGB "fake transparency" will be used by browsers which dont support rgba's "real transparency" */
     border-color: rgb(183, 183, 183);
@@ -225,12 +175,32 @@ export default {
   data() {
     return {
       file: {},
-      dropFiles: []
+      dropFiles: [
+          {
+              label: String,
+              address: String
+          }
+      ]
     };
   },
   methods: {
     deleteDropFile(index) {
       this.dropFiles.splice(index, 1);
+    },
+    onFileChange(video) {
+        var files = video.target.files || video.dataTransfer.files;
+        if(!files.length){
+            return;
+        }
+        this.createImage(files[0])
+    },
+    createVideo(file) {
+        var reader = new FileReader();
+        var vm = this;
+        reader.onload = (video) => {
+            vm.image = video.target.result;
+        };
+        reader.readAsDataURL(file);
     }
   }
 };
