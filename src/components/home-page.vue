b16447dbe79370c720214808e8189b083528c1d3
Computer <computer@Computers-MacBook-Pro.local>
2021-10-14 13:54:55 +0800

    Preset Implemented... Mostly
    Preset System is implemented, except for the preset button being used to call adding a preset, currently they are added through the console

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -48,78 +48,18 @@
         <h2 class="is-size-3 is-underlined has-text-left">Recently Used</h2>
       </div>
       <div class="columns" id="recent-section">
-        <div class="column is-2">
+        <div class="column is-2" v-for="element in test" :key="element">
             <div class="level-item has-text-centered">
               <a>
                 <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" v-on:click="getPreset(element.preset_name)">
                 </figure>
                 <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
+                  <p> {{ element.preset_name }}</p>
                 </div>
               </a>
             </div>
-          </div>
-          <div class="column is-2">
-            <div class="level-item has-text-centered">
-              <a>
-                <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-                <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="column is-2">
-            <div class="level-item has-text-centered">
-              <a>
-                <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-                <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="column is-2">
-           <div class="level-item has-text-centered">
-              <a>
-                <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-                <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="column is-2">
-            <div class="level-item has-text-centered">
-              <a>
-                <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-                <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="column is-2">
-            <div class="level-item has-text-centered">
-              <a>
-                <figure class="image is-128x128">
-                  <img id="recent-image" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                </figure>
-                <div class="content has-text-centered is-size-5" id="title-label">
-                  <p>Preset</p>
-                </div>
-              </a>
-            </div>
-          </div>
+        </div>
         </div>
     </div>
   </div>
@@ -161,6 +101,7 @@ export default ({
       dbworkstations: [],
       dbdisplays: [],
       test: [],
+      presetnum: 0,
     }
 
   },
@@ -170,7 +111,7 @@ export default ({
             this.getVideoWall();
             this.getWorkStations();
             this.getDisplays();
-            this.getPreset("Test2");
+            this.getPreset("");
         },
   methods: {
     getLights() {
@@ -282,48 +223,117 @@ export default ({
           // Check the response was a success
           if(response.data != 'undefined')
           {
-              var presetindex = NaN
-              for(let i = 0; i < response.data.length; i++){
-                if(PresetName == response.data[i].preset_name) {
-                  console.log(response.data[i].preset_name)
-                  presetindex = i
-                }
+              this.presetnum = response.data.length
+              if (PresetName != ""){
+                for(let i = 0; i < response.data.length; i++){
+                  if(PresetName == response.data[i].preset_name) {
+                    //console.log(response.data[i].preset_name)
+                    var temp = response.data[i]
+                    console.log(temp)
+                    this.UpdateDB(temp)
+                  }
               }
-              console.log(presetindex)
+              }
+              
               this.test = response.data;
               // Calls function to update lights values with db values
               //this.updateLights();
           }
       });
     },
-    pushPreset(PresetName) {
-      console.log(this.dblights)
-      axios({
-          method:'post',
-          url: 'http://127.0.0.1:8000/api/preset/',
-          data: { // Send description and status to the server
-            id: "3",
-            preset_name: PresetName,
-            lights: this.dblights,
-            video_Wall: this.dbvideo,
-            workstations: this.dbworkstations,
-            displays: this.dbdisplays,
+    UpdateDB(data) {
+      //console.log(data.lights.length)
+      for(let i = 0; i < data.lights.length; i++){
+        //console.log(data.lights[i])
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/lights/' + data.lights[i].id,
+          headers: {
+            'Content-Type': 'application/json'
           },
-          auth: { // Basic authentication
+          data: {
+            brightness: data.lights[i].brightness, 
+            colour: data.lights[i].colour,
+            id: data.lights[i].id,
+            light_on: data.lights[i].light_on,
+            name: data.lights[i].name,
+            selected: data.lights[i].selected,
+          },
+          auth: {
             username: 'admin',
             password: 'eccadmin123'
           }
-        }).then((response) => {
-          let newPreset = {'id': response.data.id, 'preset_name': this.preset_name, 'lights': this.lights, 'video_Wall': this.video_Wall, 'workstations': this.workstations, 'displays': this.displays}
-
-          this.test.push(newPreset)
         })
-        .catch((error) => {
-          console.log(error.response);
-          console.log(error.request);
-          console.log(error.message);
-        });
-    }
+      }
+      for(let i = 0; i < data.video_Wall.length; i++){
+        //console.log(data.lights[i])
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/video_wall/' + data.video_Wall[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.video_Wall[i].id,
+            source: data.video_Wall[i].source,
+            media_name: data.video_Wall[i].media_name,
+            media_type: data.video_Wall[i].media_type,
+            wall_on: data.video_Wall[i].wall_on,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      console.log(data.displays)
+      for(let i = 0; i < data.displays.length; i++){
+        //console.log(data.lights[i])
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/displays/' + data.displays[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.displays[i].id,
+            display_name: data.displays[i].display_name,
+            display_on: data.displays[i].display_on,
+            media_name: data.displays[i].media_name,
+            media_type: data.displays[i].media_type,
+            source: data.displays[i].source,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      for(let i = 0; i < data.workstations.length; i++){
+        //console.log(data.lights[i])
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/workstations/' + data.workstations[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.workstations[i].id,
+            source: data.workstations[i].source,
+            media_name: data.workstations[i].media_name,
+            media_type: data.workstations[i].media_type,
+            station_on: data.workstations[i].station_on,
+            workstation_name: data.workstations[i].workstation_name,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      
+    },
+    
 
   },
 })
