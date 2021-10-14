412b5554272c84157bd33ca4067deebd3df52c18
Computer <computer@Computers-MacBook-Pro.local>
2021-10-14 11:45:43 +0800

    Temp Commit of Preset system
    Saving state of preset system before some backend changes

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -148,3 +148,183 @@ figure:hover{
   transform: scale(1.05);
 }
 </style>
+
+<script>
+  import axios from 'axios'
+
+export default ({
+  name: 'App',
+  data() {
+    return {
+      dblights: [],
+      dbvideo: [],
+      dbworkstations: [],
+      dbdisplays: [],
+      test: [],
+    }
+
+  },
+  created() {
+            // Extracts light information stored in db
+            this.getLights();
+            this.getVideoWall();
+            this.getWorkStations();
+            this.getDisplays();
+            this.getPreset("Test2");
+        },
+  methods: {
+    getLights() {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/lights/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      // This section tells code to wait until lights have been rendered to extract db lights info
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.dblights = response.data;
+              // Calls function to update lights values with db values
+              //this.updateLights();
+          }
+      });
+    },
+    getVideoWall() {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/video_wall/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      // This section tells code to wait until lights have been rendered to extract db lights info
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.dbvideo = response.data;
+              // Calls function to update lights values with db values
+              //this.updateLights();
+          }
+      });
+    },
+    getWorkStations() {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/workstations/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      // This section tells code to wait until lights have been rendered to extract db lights info
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.dbworkstations = response.data;
+              // Calls function to update lights values with db values
+              //this.updateLights();
+          }
+      });
+    },
+    getDisplays() {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/displays/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      // This section tells code to wait until lights have been rendered to extract db lights info
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.dbdisplays = response.data;
+              // Calls function to update lights values with db values
+              //this.updateLights();
+          }
+      });
+    },
+    getPreset(PresetName) {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/preset/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      // This section tells code to wait until lights have been rendered to extract db lights info
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              var presetindex = NaN
+              for(let i = 0; i < response.data.length; i++){
+                if(PresetName == response.data[i].preset_name) {
+                  console.log(response.data[i].preset_name)
+                  presetindex = i
+                }
+              }
+              console.log(presetindex)
+              this.test = response.data;
+              // Calls function to update lights values with db values
+              //this.updateLights();
+          }
+      });
+    },
+    pushPreset(PresetName) {
+      console.log(this.dblights)
+      axios({
+          method:'post',
+          url: 'http://127.0.0.1:8000/api/preset/',
+          data: { // Send description and status to the server
+            id: "3",
+            preset_name: PresetName,
+            lights: this.dblights,
+            video_Wall: this.dbvideo,
+            workstations: this.dbworkstations,
+            displays: this.dbdisplays,
+          },
+          auth: { // Basic authentication
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        }).then((response) => {
+          let newPreset = {'id': response.data.id, 'preset_name': this.preset_name, 'lights': this.lights, 'video_Wall': this.video_Wall, 'workstations': this.workstations, 'displays': this.displays}
+
+          this.test.push(newPreset)
+        })
+        .catch((error) => {
+          console.log(error.response);
+          console.log(error.request);
+          console.log(error.message);
+        });
+    }
+
+  },
+})
+</script>
