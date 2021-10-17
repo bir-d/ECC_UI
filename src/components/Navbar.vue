d00bc9df8469470fc54c5078ac0362513bb25a8f
kshitiz675 <kshitiz@live.ca>
2021-10-17 16:45:01 +0800

    Power Button WIP

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,6 +15,11 @@
         </template>
         <template #end>
             <b-navbar-item tag="div">
+                <a class="power-button">
+                    <img src="../assets/power-button.png" v-on:click="getPreset(28)"> 
+                </a>
+            </b-navbar-item>
+            <b-navbar-item tag="div">
                 <div class="buttons">
                     <a class="button is-large">
                         <p id="button-text">+ Preset</p>
@@ -88,6 +93,126 @@ export default ({
                 console.log(error.request);
                 console.log(error.message);
                 });
+    },
+    getPreset(PresetName) {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/preset/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.presetnum = response.data.length
+              if (PresetName != ""){
+                for(let i = 0; i < response.data.length; i++){
+                  if(PresetName == response.data[i].preset_name) {
+                    var temp = response.data[i]
+                    this.UpdateDB(temp)
+                  }
+              }
+              }
+              
+              this.test = response.data;
+              
+          }
+      });
+    },
+    //Update database when a preset is called
+    UpdateDB(data) {
+      for(let i = 0; i < data.lights.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/lights/' + data.lights[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            brightness: data.lights[i].brightness, 
+            colour: data.lights[i].colour,
+            id: data.lights[i].id,
+            light_on: data.lights[i].light_on,
+            name: data.lights[i].name,
+            selected: data.lights[i].selected,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      for(let i = 0; i < data.video_Wall.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/video_wall/' + data.video_Wall[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.video_Wall[i].id,
+            source: data.video_Wall[i].source,
+            media_name: data.video_Wall[i].media_name,
+            media_type: data.video_Wall[i].media_type,
+            wall_on: data.video_Wall[i].wall_on,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      console.log(data.displays)
+      for(let i = 0; i < data.displays.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/displays/' + data.displays[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.displays[i].id,
+            display_name: data.displays[i].display_name,
+            display_on: data.displays[i].display_on,
+            media_name: data.displays[i].media_name,
+            media_type: data.displays[i].media_type,
+            source: data.displays[i].source,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      for(let i = 0; i < data.workstations.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/workstations/' + data.workstations[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.workstations[i].id,
+            source: data.workstations[i].source,
+            media_name: data.workstations[i].media_name,
+            media_type: data.workstations[i].media_type,
+            station_on: data.workstations[i].station_on,
+            workstation_name: data.workstations[i].workstation_name,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      
     }
     }
 })
