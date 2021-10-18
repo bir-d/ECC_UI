7039c8876b2e514d6b010e12e02b7d8b8993ad31
bird <20701908+bir-d@users.noreply.github.com>
2021-10-18 11:28:46 +0800

    Allow presets page to actually load presets

--- src/components/presets.vue
+++ src/components/presets.vue
@@ -10,7 +10,7 @@
             <div class="level-item has-text-centered">
               <a>
                 <figure class="image is-128x128">
-                  <img id="recent-image" class="is-large is-rounded"  src="https://bulma.io/images/placeholders/480x480.png" v-on:click="getPreset(element.preset_name)">
+                  <img id="recent-image" class="is-large is-rounded"  src="https://bulma.io/images/placeholders/480x480.png" v-on:click="getPreset(element.preset_name, true)">
                 </figure>
                 
                 <div class="content has-text-centered is-size-5" id="title-label">
@@ -98,6 +98,50 @@ export default ({
       });
     },
 
+    getPreset(PresetName, launchNotification = false) {
+      axios({
+          method:'get',
+          // Url of backend location of data
+          url: 'http://127.0.0.1:8000/api/preset/',
+          auth: {
+              username: 'admin',
+              password: 'eccadmin123'
+          }
+      
+      }).then((response) => {
+
+          this.isLoaded = true;
+
+          // Check the response was a success
+          if(response.data != 'undefined')
+          {
+              this.presetnum = response.data.length
+              if (PresetName != ""){
+                for(let i = 0; i < response.data.length; i++){
+                  if(PresetName == response.data[i].preset_name) {
+                    var temp = response.data[i]
+                    this.UpdateDB(temp)
+                  }
+              }
+              }
+              
+              this.test = response.data;
+              
+          }
+      });
+
+      // only if notification specified
+      if (launchNotification){
+          this.$buefy.notification.open({
+              message: 'Preset "' + PresetName +'" was successfully loaded!',
+              duration: 5000,
+              position: "is-bottom-right",
+              type: 'is-success',
+              hasIcon: true,
+              queue: false
+          })
+      }
+    },
   },
 })
 </script>
