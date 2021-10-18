6f889b0c89f03ba944ec9d51f7e29ac7d7e0ac4d
Computer <computer@Computers-MacBook-Pro.local>
2021-10-18 11:59:56 +0800

    Interium commit

--- src/components/presets.vue
+++ src/components/presets.vue
@@ -63,44 +63,105 @@ export default ({
       presets: [],
       presetnum: 0,
       presetrange: [],
+      test: [],
     }
 
   },
   created() {
             // Extracts ECC information stored in db
-            this.getPresets();
+            this.getPreset("");
             //this.PresetIndex();
         },
   methods: {
-    getPresets() {
-      axios({
-          method:'get',
-          // Url of backend location of data
-          url: 'http://127.0.0.1:8000/api/preset/',
+    //Update database when a preset is called
+    UpdateDB(data) {
+      for(let i = 0; i < data.lights.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/lights/' + data.lights[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            brightness: data.lights[i].brightness, 
+            colour: data.lights[i].colour,
+            id: data.lights[i].id,
+            light_on: data.lights[i].light_on,
+            name: data.lights[i].name,
+            selected: data.lights[i].selected,
+          },
           auth: {
-              username: 'admin',
-              password: 'eccadmin123'
+            username: 'admin',
+            password: 'eccadmin123'
           }
+        })
+      }
+      for(let i = 0; i < data.video_Wall.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/video_wall/' + data.video_Wall[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.video_Wall[i].id,
+            source: data.video_Wall[i].source,
+            media_name: data.video_Wall[i].media_name,
+            media_type: data.video_Wall[i].media_type,
+            wall_on: data.video_Wall[i].wall_on,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      for(let i = 0; i < data.displays.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/displays/' + data.displays[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.displays[i].id,
+            display_name: data.displays[i].display_name,
+            display_on: data.displays[i].display_on,
+            media_name: data.displays[i].media_name,
+            media_type: data.displays[i].media_type,
+            source: data.displays[i].source,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
+      for(let i = 0; i < data.workstations.length; i++){
+        axios({
+          method:'put',
+          url: 'http://127.0.0.1:8000/api/workstations/' + data.workstations[i].id,
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            id: data.workstations[i].id,
+            source: data.workstations[i].source,
+            media_name: data.workstations[i].media_name,
+            media_type: data.workstations[i].media_type,
+            station_on: data.workstations[i].station_on,
+            workstation_name: data.workstations[i].workstation_name,
+          },
+          auth: {
+            username: 'admin',
+            password: 'eccadmin123'
+          }
+        })
+      }
       
-      }).then((response) => {
-
-          this.isLoaded = true;
-
-          // Check the response was a success
-          if(response.data != 'undefined')
-          {
-              
-            this.presets = response.data;
-            this.presetnum = response.data.length
-            for(let i = 0; i < this.presetnum; i+=5){
-                this.presetrange.push([i,i+5])
-            }
-              
-          }
-      });
     },
-
     getPreset(PresetName, launchNotification = false) {
+        console.log(PresetName)
       axios({
           method:'get',
           // Url of backend location of data
@@ -118,6 +179,7 @@ export default ({
           if(response.data != 'undefined')
           {
               this.presetnum = response.data.length
+              console.log()
               if (PresetName != ""){
                 for(let i = 0; i < response.data.length; i++){
                   if(PresetName == response.data[i].preset_name) {
